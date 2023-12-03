4013a4cd6d57adfe92922b83c2b5570332ba9037
ingkellswith <allover3773@gmail.com>
2023-12-04 02:08:20 +0900

    상품 찾기 기능 구현

---
+++ src/main/java/design/database/apple/mapper/ProductMapper.java
@@ -0,0 +1,15 @@
+package design.database.apple.mapper;
+
+import design.database.apple.model.User;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface ProductMapper {
+
+    List<HashMap<String, String>> getFixedDepositProductList();
+    List<HashMap<String, String>> getDepositProductList();
+    List<HashMap<String, String>> getCheckingAccountProductList();
+    List<HashMap<String, String>> getLoanProductList();
+    List<HashMap<String, String>> getCardProductList();
+}
